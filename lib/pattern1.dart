6d89867cc1ec7c2e62676c61a928f96192f09862
fnaja <fnaja688@gmail.com>
2024-07-25 20:26:35 +0530

    25/07/24

--- lib/pattern1.dart
+++ lib/pattern1.dart
@@ -1,20 +1,26 @@
 import 'dart:io';
-void main()
-{
-  int i,j;
+void main() {
+  int i, j;
   print("enter a number");
-  var d=stdin.readLineSync();
-  int n=int.parse(d!);
-  for(i=0;i<=n;i++)
-    {
-      for(j=0;j<=i;j++)
-        {
-          stdout.write(" ");
-        }
-      for(j=0;j<=n;j++)
-        {
-          stdout.write("*");
-        }
+  var d = stdin.readLineSync();
+  int n = int.parse(d!);
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <=n-i; j++) {
+      stdout.write(" ");
     }
-  stdout.write("\n");
+    {
+      for (j = 1; j <= n ; j++)
+        stdout.write("*");
+    }
+    stdout.write("\n");
+  }
+  for (i= n; i <= n-i; i++) {
+    for (j = 1; j <= n; j++) {
+      stdout.write(" ");
+    }
+    for (j = 1; j <= n; j++) {
+      stdout.write("*");
+    }
+    stdout.write("\n");
+  }
 }
