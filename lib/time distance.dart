8f8c3b26f86ea67bb5a75178dc2ea394e548c837
fnaja <fnaja688@gmail.com>
2024-07-29 20:28:44 +0530

    29/07/2024

--- lib/time distance.dart
+++ lib/time distance.dart
@@ -1,7 +1,9 @@
 import'dart:io';
 void main() {
-  double distance= 25.0;
-  double speed = 40.0;
+  print("enter the distance:");
+  double distance=double.parse(stdin.readLineSync()!);
+  print("enter the people:");
+  double  speed=double.parse(stdin.readLineSync()!);
   double time_hours = distance / speed;
   double time_minutes = time_hours * 60.0;
   print("Time taken to reach the office: $time_minutes");
