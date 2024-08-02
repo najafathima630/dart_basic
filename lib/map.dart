096402f82ea9b21dcc6f4361159db3b3d96bc4b1
fnaja <fnaja688@gmail.com>
2024-08-02 20:07:52 +0530

    02/08/2024

--- lib/map.dart
+++ lib/map.dart
@@ -5,11 +5,21 @@ void main()
   print(details);
   details ={"name":"noora"};
   print(details);
-  // details{"age="21};
-  // print(details);
+  details['age'] ='20';// adding key in the old key
+  print(details);
   var student=new Map();//using constractor method
-  student={"name":"anjana","place":"calicut",};
+  student={"name":"anjana","place":"calicut",};//constrouter method adding the data
   print(student);
-  student.remove("place");
+  student.remove("place");//remove key value
   print(student);
+  var a={"name":"naja","place":"naduvannur"};
+  print(a);
+  a.clear;//full clear the data
+  if (student.isEmpty) { //true or false condition chek
+    print("isEmpty");
+  }
+  else{
+  print( "isNotEmpty");
+
+}
   }
