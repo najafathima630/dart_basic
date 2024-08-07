b81a412ae825e726e3b7df577373456aae1a8d5b
fnaja <fnaja688@gmail.com>
2024-08-07 12:28:23 +0530

    07/08/2024

---
+++ lib/mixin.dart
@@ -0,0 +1,25 @@
+void main() {
+  var abc= Abc();
+  abc.Apple();
+  abc.Kiwi();
+
+
+}
+mixin apple {
+  void Apple() {
+    print('this is a king of fruits');
+  }
+}
+mixin kiwi
+{
+  void Kiwi(){
+    print('This is a fruits');
+  }
+}
+
+
+
+class Abc with apple, kiwi {//mixin name calling in class
+
+}
+
